3d8333e89701f6516f9a4a9f01788fbfe53c5e4e
Shawon-Ahmed <sa735392@gmail.com>
2024-12-26 22:06:57 +0600

    remove

--- resources/views/frontend/business.php
+++ resources/views/frontend/business.php
@@ -1,7 +1,3 @@
 <div>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio neque hic nesciunt natus. Nobis voluptatibus nisi maiores labore dignissimos, voluptates illo! Quos velit rem recusandae non consectetur laboriosam corporis voluptatum, quo dolores ducimus alias saepe libero ratione eveniet vel voluptatibus, et adipisci amet. Unde explicabo earum repellendus quae autem, animi veniam nulla debitis vel enim ea eius harum officiis neque nam voluptatem doloribus facere a ullam, id velit assumenda magnam magni. Facere dignissimos sequi expedita iusto qui unde quo! Unde cumque recusandae, quo provident sint adipisci tenetur inventore quaerat atque a alias autem molestiae tempora? Incidunt nobis nostrum facilis corporis.
-	</p>
 
-	<input type="text" name="" id="">
-	<button type="submit">Shawon</button>
 </div>
